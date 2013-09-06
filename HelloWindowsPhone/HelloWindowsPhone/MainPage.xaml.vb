4d52b417de0b83bc39f0b04ee393d3ea7f970d9d
Rob <rjauquet@gmail.com>
2013-09-06 10:34:15 -0500

    adding projects

---
+++ HelloWindowsPhone/HelloWindowsPhone/MainPage.xaml.vb
@@ -0,0 +1,18 @@
+﻿Partial Public Class MainPage
+    Inherits PhoneApplicationPage
+
+    ' Constructor
+    Public Sub New()
+        InitializeComponent()
+        TextBox1.Text = "Hello World"
+        TextBlock1.Text = ""
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.Windows.RoutedEventArgs) Handles Button1.Click
+        TextBlock1.Text = TextBox1.Text
+    End Sub
+
+    Private Sub HyperlinkButton1_Click(sender As System.Object, e As System.Windows.RoutedEventArgs) Handles HyperlinkButton1.Click
+        NavigationService.Navigate(New Uri("/SecondPage.xaml", UriKind.RelativeOrAbsolute))
+    End Sub
+End Class
---
+++ HelloWindowsPhone/HelloWindowsPhone/SecondPage.xaml.vb
@@ -0,0 +1,7 @@
+﻿Partial Public Class SecondPage
+    Inherits PhoneApplicationPage
+
+    Public Sub New()
+        InitializeComponent()
+    End Sub
+End Class
